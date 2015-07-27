a9c855b5c56170e297eaa3570973e7329f1b4227
Ludwik <ludwik.bukowski@gmail.com>
2015-07-27 09:37:20 +0200

    Fixed C files compilation with rebar3

--- tools/get_included_files_h.erl
+++ tools/get_included_files_h.erl
@@ -1,11 +1,13 @@
 #!/usr/bin/env escript
 
-path_erts()->
-  Erts_dir = lists:concat([code:root_dir(), "/erts-", erlang:system_info(version)]),
-  filename:join(Erts_dir, "include").
-path_interface()->
+path_erts() ->
+  ErtsDir = lists:concat([code:root_dir(), "/erts-", erlang:system_info(version)]),
+  filename:join(ErtsDir, "include").
+
+path_interface() ->
   code:lib_dir(erl_interface, include).
-path()->lists:concat(["-I ",path_erts()," -I ",path_interface()]).
+
+path() -> lists:concat(["-I ", path_erts(), " -I ", path_interface()]).
+
 main(_) ->
   io:format(path()).
-
