672dcded6f0a447404556a35dff24be348281a0d
Ludwik <ludwik.bukowski@gmail.com>
2015-07-29 11:32:12 +0200

    cleaning

--- c_src/exml_escape.d
+++
@@ -1,6 +0,0 @@
-c_src/exml_escape.o: c_src/exml_escape.c c_src/exml_escape.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_nif.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_native_features_config.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_drv_nif.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_int_sizes_config.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_nif_api_funcs.h
--- c_src/exml_event.d
+++
@@ -1,6 +0,0 @@
-c_src/exml_event.o: c_src/exml_event.c c_src/exml_event.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_nif.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_native_features_config.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_drv_nif.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_int_sizes_config.h \
-  /Users/ludwikbukowski/Desktop/Projekt/erlang/erts-6.4/include/erl_nif_api_funcs.h
