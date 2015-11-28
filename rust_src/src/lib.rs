f1825c6ea454c8447fae923eb09f8b45afe8cb00
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-11-28 18:54:04 +0000

    Parse element attributes
    EUnit test exml_event_tests:attrs_parsing_test/0 passes!

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -294,14 +294,15 @@ fn parse_nif(env: *mut ErlNifEnv,
     for ev in parser {
         match ev {
             // TODO: Suboptimal! Binaries should be put on a stack here...
-            Ok (XMLEvent::StartElement { name, .. }) => {
+            Ok (XMLEvent::StartElement { name, attributes, .. }) => {
                 let bname = nif_try!(Binary::from_string(env, &name.local_name)
                                             .and_then(|b| b.to_term(env)));
                 let empty_list = nif_try!(List(&[]).to_term(env));
+                let attrs = nif_try!(attribute_list(env, &attributes));
                 let tuple = nif_try!(Tuple(&[atom::xml_element_start(env),
                                              bname,
                                              empty_list,
-                                             empty_list]).to_term(env));
+                                             attrs]).to_term(env));
                 events.push(tuple);
             }
             // TODO: ...and popped off this stack here.
@@ -349,6 +350,20 @@ impl<'a> List<'a> {
             Ok (list)
         }
     }
+}
+
+fn attribute_list(env: *mut ErlNifEnv,
+                  attrs: &[xml::attribute::OwnedAttribute]) -> Result<ERL_NIF_TERM, Error> {
+    let l: Vec<ERL_NIF_TERM> = attrs.iter()
+        .map(|oa| {
+            let attr = [nif_try!(Binary::from_string(env, &oa.name.local_name)
+                                        .and_then(|b| b.to_term(env))),
+                        nif_try!(Binary::from_string(env, &oa.value)
+                                        .and_then(|b| b.to_term(env)))];
+            nif_try!(Tuple(&attr).to_term(env))
+        })
+        .collect();
+    List(&l).to_term(env)
 }
 
 fn indent(size: usize) -> String {
