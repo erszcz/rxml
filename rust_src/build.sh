cea92780a1c672a7a8dc9683d556442d78bde792
Radek Szymczyszyn <lavrin@gmail.com>
2015-12-13 19:52:22 +0100

    Enable printing warnings from build.sh

--- rust_src/build.sh
+++ rust_src/build.sh
@@ -1,1 +1,4 @@
-cd ../ && make || cat rust_src/build.log; cd -
+cd ../ && make
+#(test $? != 0 || grep -i warning rust_src/build.log) && cat rust_src/build.log
+(test $? != 0) && cat rust_src/build.log
+cd -
